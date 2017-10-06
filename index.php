dc598dd54749939498b2b6a8496cb903ce792952
Sourabh-Rupani <saurabh_rupani@yahoo.com>
2017-10-06 14:43:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Environs Movement</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="header-left">
+          <img class="logo" src="">
+        </div>
+        <div class="header-right">
+          <a href="#" class="login">Log in</a>
+        </div>
+      </div>
+    </header>
+    <div class="top-wrapper">
+      <div class="container">
+        
+      </div>
+    </div>
+    <div class="lesson-wrapper">
+      <div class="container">
+        <div class="heading">
+          
+        </div>
+        <div class="lessons">
+          
+        </div>
+      </div>
+    </div>
+    <div class="message-wrapper">
+      <div class="container">
+        <div class="heading">
+          
+        </div>
+        </div>
+    </div>
+    <footer>
+      <div class="container">
+       
+      </div>
+    </footer>
+  </body>
+</html>
